81fd6d5d0aad2353ba51697c41d118a3ee676045
pedro <pedroluismontesinosrivera@gmail.com>
2018-05-05 23:02:00 -0400

    added chart animation

--- js/script.js
+++ js/script.js
@@ -13,32 +13,36 @@ $(document).ready(function(){
 		showCursor: false
 	});
 
-	var owl = $('.owl-carousel');
-		owl.owlCarousel({
-		    loop:true,
-		    nav:true,
-		    margin:10,
-		    responsive:{
-		        0:{
-		            items:1
-		        },
-		        600:{
-		            items:3
-		        },            
-		        960:{
-		            items:5
-		        },
-		        1200:{
-		            items:6
-		        }
-		    }
-		});
-		owl.on('mousewheel', '.owl-stage', function (e) {
-		    if (e.deltaY>0) {
-		        owl.trigger('next.owl');
-		    } else {
-		        owl.trigger('prev.owl');
-		    }
-		    e.preventDefault();
-		});
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items:4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	})
+
+	$(function() {
+        $('.chart').easyPieChart({
+            easing: 'easeInOut',
+            barColor: '#fff',
+            trackColor:false,
+            scaleColor:false,
+            lineWidth:4,
+            size:152,
+            onStep: function(from, to, percent){
+            	$(this.el).find('.percent').text(Math.round(percent));
+            }
+        });
+    });
 });
