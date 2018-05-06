556cd8879875fa46cddeffaab8f891a2e9bd94f2
pedro <pedroluismontesinosrivera@gmail.com>
2018-05-05 21:19:47 -0400

    added about section

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
 		play: 5000,
 		pagination:false
 	});
+
+	var typed = new Typed('.typed', {
+		strings: ['Web Developer', 'Self-taught programmer' ],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
 });
